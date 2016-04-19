a04b3b9b2588644391a27a6476ea65c2ecb00d03
jarshuy <13340338233@sina.cn>
2016-04-19 11:52:23 +0800

    dellpc summit

--- art/runtime/gc/heap.cc
+++ art/runtime/gc/heap.cc
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2011 The Android Open Source Project
- *
+ *"It's time to change the source code-zjs-4-19-dellpc"
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
